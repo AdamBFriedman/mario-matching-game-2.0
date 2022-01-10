9354f744bb6952f977a1bd6ff98338bf82abda59
Adam Friedman <adam.friedman5@gmail.com>
2022-01-09 23:22:42 -0600

    Clicking card reveals image.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,75 @@
 import "./App.css";
-import gameCards from "./gameCards.json";
+import { gameCards } from "./gameCards";
 
 function App() {
+  // Double the size of the grid
   const gameGrid = gameCards.concat(gameCards).sort(() => 0.5 - Math.random());
+  let firstGuess = "";
+  let secondGuess = "";
+  let count = 0;
+  let previousTarget = null;
+  let delay = 1200;
+
+  const match = () => {
+    const selected = document.querySelectorAll(".selected");
+    selected.forEach((card) => {
+      card.classList.add("match");
+    });
+  };
+
+  const resetGuesses = () => {
+    firstGuess = "";
+    secondGuess = "";
+    count = 0;
+    previousTarget = null;
+
+    let selected = document.querySelectorAll(".selected");
+    selected.forEach((card) => {
+      card.classList.remove("selected");
+    });
+  };
+
+  const handleClick = (event) => {
+    const clicked = event.target;
+
+    if (
+      clicked.nodeName === "SECTION" ||
+      clicked === previousTarget ||
+      clicked.parentNode.classList.contains("selected") ||
+      clicked.parentNode.classList.contains("match")
+    ) {
+      return;
+    }
+
+    if (count < 2) {
+      count++;
+      if (count === 1) {
+        firstGuess = clicked.parentNode.dataset.name;
+        clicked.parentNode.classList.add("selected");
+      } else {
+        secondGuess = clicked.parentNode.dataset.name;
+        clicked.parentNode.classList.add("selected");
+      }
+
+      if (firstGuess && secondGuess) {
+        if (firstGuess === secondGuess) {
+          setTimeout(match, delay);
+        }
+        setTimeout(resetGuesses, delay);
+      }
+      previousTarget = clicked;
+    }
+  };
   return (
     <section className="grid">
       {gameGrid.map((card) => (
-        <div data-name={card.name} className="card">
+        <div onClick={handleClick} data-name={card.name} className="card">
           {card.name}
           <div className="front"></div>
-          <div className="back"></div>
+          <div
+            className="back"
+            style={{ backgroundImage: `url(${card.image})` }}
+          ></div>
         </div>
       ))}
     </section>
---
+++ src/gameCards.js
@@ -0,0 +1,50 @@
+export const gameCards = [
+  {
+    name: "kamek",
+    image: require("./images/kamek.png"),
+  },
+  {
+    name: "boo",
+    image: require("./images/boo.png"),
+  },
+  {
+    name: "bobomb",
+    image: require("./images/bobomb.png"),
+  },
+  {
+    name: "birdo",
+    image: require("./images/birdo.png"),
+  },
+  {
+    name: "cheepcheep",
+    image: require("./images/cheepcheep.png"),
+  },
+  {
+    name: "koopa",
+    image: require("./images/koopa.png"),
+  },
+  {
+    name: "shyguy",
+    image: require("./images/shyguy.png"),
+  },
+  {
+    name: "mushroom",
+    image: require("./images/mushroom.png"),
+  },
+  {
+    name: "thwomp",
+    image: require("./images/thwomp.png"),
+  },
+  {
+    name: "bulletbill",
+    image: require("./images/bulletbill.png"),
+  },
+  {
+    name: "lakitu",
+    image: require("./images/lakitu.png"),
+  },
+  {
+    name: "goomba",
+    image: require("./images/goomba.png"),
+  },
+];
